f4eeeb8202ac871377d3ef99857a752c3985e446
nathan shearer <help@rapidstartup.io>
2025-03-10 13:31:31 +0800

    allow supabase access to forge env

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -8,7 +8,10 @@ module.exports = {
     max_memory_restart: "1G",
     env: {
       NODE_ENV: "production",
-      PORT: 3002
+      PORT: 3002,
+      VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
+      VITE_SUPABASE_SERVICE_KEY: process.env.VITE_SUPABASE_SERVICE_KEY,
+      VITE_SOCKET_SERVER_URL: process.env.VITE_SOCKET_SERVER_URL || "https://io.click2call.ai:3002"
     }
   }]
 }; 
